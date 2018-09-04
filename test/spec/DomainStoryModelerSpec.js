f446a9b2afefbfa6784136870054427d699570dd
Alexander <alexander.zellober@wps.de>
2018-09-04 16:28:28 +0200

    az: added polyfill for string.endWith function

--- test/spec/DomainStoryModelerSpec.js
+++ test/spec/DomainStoryModelerSpec.js
@@ -20,10 +20,21 @@ describe('domainStory modeler', function() {
 
   describe('domainStory import export', function() {
 
-    // since PhantomJS does not implement ES6 features we have to define our own string.includes method
+    // since PhantomJS does not implement ES6 features we have to define our own string.includes and string.endsWith methods
     if (!String.prototype.includes) {
       String.prototype.includes = function() {'use strict';
         return String.prototype.indexOf.apply(this, arguments) !== -1;
+      };
+    }
+    if (!String.prototype.endsWith) {
+      String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+          position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
       };
     }
 
