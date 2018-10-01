31e8569405407aaf409f76d2d8a2716f81fa6fed
Alexander <alexander.zellober@wps.de>
2018-10-01 11:23:01 +0200

    removed unused code

--- app/domain-story-modeler/domain-story/modeling/DSModeling.js
+++ app/domain-story-modeler/domain-story/modeling/DSModeling.js
@@ -9,42 +9,6 @@ export default function DSModeling(eventBus, elementFactory, commandStack,
   Modeling.call(this, eventBus, elementFactory, commandStack, domainStoryRules);
 }
 
-
-// Modeling.prototype.updateLabel = function(element, newLabel, newBounds) {
-
-//   if (/^domainStory:/.test(element.type)) {
-//     this._commandStack.execute('element.updateCustomLabel', {
-//       element: element,
-//       newLabel: newLabel,
-//       newBounds: newBounds
-//     });
-//   } else {
-//     this._commandStack.execute('element.updateLabel', {
-//       element: element,
-//       newLabel: newLabel,
-//       newBounds: newBounds
-//     });
-
-//   }
-// };
-
-// Modeling.prototype.updateNumber= function(element, newNumber, newBounds) {
-
-//   if (/^domainStory:/.test(element.type)) {
-//     this._commandStack.execute('element.updateCustomLabel', {
-//       element: element,
-//       newNumber: newNumber,
-//       newBounds: newBounds
-//     });
-//   } else {
-//     this._commandStack.execute('element.updateLabel', {
-//       element: element,
-//       newNumber: newNumber,
-//       newBounds: newBounds
-//     });
-//   }
-// };
-
 Modeling.prototype.replaceShape = function(oldShape, newShape, hints) {
   var context = {
     oldShape: oldShape,
