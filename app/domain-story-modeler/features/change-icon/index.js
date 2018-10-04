ef0950561df30353bfad242c4717540dd5acaf76
Stefan Hofer <sh@wps.de>
2018-10-04 09:42:03 +0200

    removes unused dependencies

--- app/domain-story-modeler/features/change-icon/index.js
+++ app/domain-story-modeler/features/change-icon/index.js
@@ -1,16 +1,11 @@
 'use strict';
 
-import PopupMenuModule from 'diagram-js/lib/features/popup-menu';
-import ReplaceModule from 'bpmn-js/lib/features/replace';
-
 import ReplaceMenuProvider from './ReplaceMenuProvider';
 import DSModeling from '../modeling/DSModeling';
 
 
 export default {
   __depends__: [
-    PopupMenuModule,
-    ReplaceModule
   ],
   __init__: [ 'replaceMenuProvider' ],
   replaceMenuProvider: [ 'type', ReplaceMenuProvider ],
