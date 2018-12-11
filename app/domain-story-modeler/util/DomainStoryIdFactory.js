ae2c7033c817031a27b84a1a0d858bd8b77f4069
Alexander Zellober <alexander.zellober@wps.de>
2018-12-11 14:58:43 +0100

    bugfix in number generation and added test to ensure unique ID-creation #31

--- app/domain-story-modeler/util/DomainStoryIdFactory.js
+++ app/domain-story-modeler/util/DomainStoryIdFactory.js
@@ -15,35 +15,51 @@ DomainStoryIdFactory.prototype.registerId = function(id) {
 
 function generateId(type) {
 
-  var id = type + '_' + fourDigitsId();
+  var idNumber =fourDigitsId();
+
+  var id = type + '_' + idSuffix(idNumber);
 
   while (containsId(id)) {
-    id = type + '_' + fourDigitsId();
+    idNumber++;
+
+    id = type + '_' + idSuffix(idNumber);
   }
 
   idList.push(id);
   return id;
 }
 
-function fourDigitsId() {
-  var idNumber= Math.floor(Math.random() * 10000);
-  if (idNumber < 10) {
-    idNumber= '000' + idNumber;
+function idSuffix(idNumber) {
+  var id = '';
+  if (idNumber > 9999) {
+    id = 0;
+  }
+  else if (idNumber < 10) {
+    id= '000' + idNumber;
   }
   else if (idNumber < 100) {
-    idNumber = '00' + idNumber;
+    id = '00' + idNumber;
   }
   else if (idNumber < 1000) {
-    idNumber = '0' + idNumber;
+    id = '0' + idNumber;
   }
+  else {
+    id = '' + idNumber;
+  }
+  return id;
+}
+
+function fourDigitsId() {
+  var idNumber = Math.floor(Math.random() * 10000);
   return idNumber;
 }
 
 function containsId(id) {
+  var same = false;
   idList.forEach(element => {
-    if (id == element) {
-      return true;
+    if (id === element) {
+      same = true;
     }
   });
-  return false;
+  return same;
 }
--- test/spec/language/DomainStoryIdFactorySpec.js
+++ test/spec/language/DomainStoryIdFactorySpec.js
@@ -28,4 +28,19 @@ describe('DomainStoryIdFactory', function() {
     var otherId = 'shape_00001';
     assert.notDeepEqual(res.length, otherId.length);
   });
+
+  it('generateId unique iD amount 5k', function() {
+    this.timeout(0);
+    var idFactory = new DomainStoryIdFactory();
+    var res = [];
+    for (var i=0; i<5000;i++) {
+      res[i] =idFactory.getId('');
+    }
+
+    for (i=0; i<res.length-1;i++) {
+      for (var j=i+1; j<res.length;j++) {
+        assert.notEqual(res[i], res[j]);
+      }
+    }
+  });
 });
