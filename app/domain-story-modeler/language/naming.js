6cb1d8c8896d74c28e88746a3213c234a11035c2
Alexander Zellober <alexander.zellober@wps.de>
2018-12-13 19:30:43 +0100

    adds polyfill for testing

--- app/domain-story-modeler/language/naming.js
+++ app/domain-story-modeler/language/naming.js
@@ -2,6 +2,23 @@
 'use strict';
 
 export function getNameFromType(type) {
+
+  // Polyfill for test
+  if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+      'use strict';
+      if (typeof start !== 'number') {
+        start = 0;
+      }
+
+      if (start + search.length > this.length) {
+        return false;
+      } else {
+        return this.indexOf(search, start) !== -1;
+      }
+    };
+  }
+
   if (type.includes('domainStory:actor')) {
     return type.replace('domainStory:actor', '');
   }
