bc737c81fd7c2ab80342ace1439725c38eb94361
Alexander Zellober <alexander.zellober@wps.de>
2018-12-18 12:03:48 +0100

    fixes rest of icons

--- app/domain-story-modeler/language/iconConfig.js
+++ app/domain-story-modeler/language/iconConfig.js
@@ -10,12 +10,66 @@ export function getIconset() {
 /**
  * Define your custom Iconsets here
  */
-var custom_conf = {
+
+var custom_icons = {
   'actors': [
-    'Person'
+
   ],
   'workObjects': [
-    'Flag'
+
+  ]
+};
+
+/**
+  * All Icons as one Set
+  * There are more Icons than fit in the palette.
+  * This is just for reference
+  */
+
+var allIcons_conf = {
+  'actors': [
+    'Person',
+    'Group',
+    'System',
+    'Pet',
+  ],
+  'workObjects': [
+    'Place',
+    'Flag', 'World', 'Water',
+    'Store', 'Theater', 'Business', 'Meeting-room',
+    'Hotel', 'Dining', 'Courthouse',
+    'Gas-station',
+    'Car', 'Bus', 'Train', 'Truck', 'Taxi', 'Bike', 'Boat', 'Motorcycle',
+    'Plane', 'Flight-takeoff', 'Flight-landing',
+    'Shuttle', 'Walking', 'Traffic', 'Commute',
+    'Document', 'Folder', 'Call', 'Email', 'Copyright', 'Briefcase', 'Attach', 'Ruler',
+    'Sum',
+    'Conversation',
+    'Update',
+    'Cellphone', 'Speaker-phone',
+    'Signal',
+    'Key',
+    'Pencil',
+    'Settings', 'Save', 'Delete', 'Wrench',
+    'Headset', 'Keyboard', 'Mouse', 'Microphone',
+    'Router', 'Scanner', 'Printer', 'DNS',
+    'Security', 'Cloud',
+    'TV', 'Video', 'Desktop', 'Tablet',
+    'Assessment', 'Dashboard',
+    'Pie-chart', 'Insert-chart', 'View-List',
+    'Euro', 'Dollar',
+    'Info', 'Alarm', 'Problem',
+    'Circle-Arrows',
+    'Picture-as-PDF',
+    'Credit-Card', 'Shopping',
+    'Favorite',
+    'Gavel',
+    'Blind',
+    'Hourglass', 'Time',
+    'Search',
+    'Thumb-up', 'Thumb-down', 'Thumb-up-down',
+    'Couch',
+    'Education',
   ]
 };
 
