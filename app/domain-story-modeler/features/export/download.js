c1e5381d13de42178729d4550c0fdb3c4a88c051
Alexander Zellober <alexander.zellober@wps.de>
2019-01-17 11:20:55 +0100

    escapes # characters in svg-uri during png-generation for newer browser versions

--- app/domain-story-modeler/features/export/download.js
+++ app/domain-story-modeler/features/export/download.js
@@ -54,11 +54,54 @@ export function downloadPNG() {
 
   top = prepareSVG(top);
 
+  top = URIHashtagFix(top);
+
   image.width = width;
   image.height = height;
-  // since svg references the markers polylines with a # symbol, we get a depreciation-warning for having # in the data-URI
   image.src=('data:image/svg+xml,' + top);
 }
+
+function URIHashtagFix(top) {
+  var fix = false;
+  var browser = navigator.browserSpecs;
+  console.log(browser);
+  var name = browser.name;
+  var version = browser.version;
+
+  // only implemented in chrome and firefox at the moment
+  if (name.includes('Chrome')) {
+    if (version >= 72) {
+      fix = true;
+    }
+  }
+  else if (name.includes('Firefox')) {
+    fix = true;
+    // versionNumber of implementation unknown
+  }
+  if (fix) {
+    while (top.includes('#')) {
+      top=top.replace('#', '%23');
+    }
+  }
+  return top;
+}
+
+navigator.browserSpecs = (function() {
+  var ua = navigator.userAgent, tem,
+      M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return { name:'IE',version:(tem[1] || '') };
+  }
+  if (M[1]=== 'Chrome') {
+    tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+    if (tem != null) return { name:tem[1].replace('OPR', 'Opera'),version:tem[2] };
+  }
+  M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i))!= null)
+    M.splice(1, 1, tem[1]);
+  return { name:M[0], version:M[1] };
+})();
 
 export function prepareSVG(top) {
   var bounds = '';
