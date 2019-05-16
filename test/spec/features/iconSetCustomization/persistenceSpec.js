4be781e5fb1e4537d89d09f1e556e73ab3349e2b
Alexander Zellober <alexander.zellober@wps.de>
2019-05-16 17:37:34 +0200

    fixes persitenceTest to check for correct Entries in generated config

--- test/spec/features/iconSetCustomization/persistenceSpec.js
+++ test/spec/features/iconSetCustomization/persistenceSpec.js
@@ -1,19 +1,17 @@
-import { addToSelectedActors, addToSelectedWorkObjects, getSelectedActorsDictionary, getSelectedWorkObjectsDictionary } from '../../../../app/domain-story-modeler/features/iconSetCustomization/dictionaries';
-import { initTypeDictionaries, getTypeIconSRC } from '../../../../app/domain-story-modeler/language/icon/dictionaries';
-import { default_conf } from '../../../../app/domain-story-modeler/language/icon/iconConfig';
+import { addToSelectedActors, getSelectedActorsDictionary, getSelectedWorkObjectsDictionary, addToSelectedWorkObjects } from '../../../../app/domain-story-modeler/features/iconSetCustomization/dictionaries';
+import { getTypeIconSRC, initTypeDictionaries } from '../../../../app/domain-story-modeler/language/icon/dictionaries';
 import { ACTOR, WORKOBJECT } from '../../../../app/domain-story-modeler/language/elementTypes';
 import { createConfigFromDictionaries } from '../../../../app/domain-story-modeler/features/iconSetCustomization/persitence';
+import { default_conf } from '../../../../app/domain-story-modeler/language/icon/iconConfig';
 
-initTypeDictionaries(default_conf.actors, default_conf.workObjects);
-
-describe('', function() {
-  let actorName = 'Pet',
-      workObjectName = 'Flag';
+describe('persitence ', function() {
+  const actorName = 'Pet', workObjectName = 'Flag';
 
   it('createConfigFromDictionaries', function() {
     // Given
-    addToSelectedActors(actorName, getTypeIconSRC(ACTOR, actorName));
-    addToSelectedWorkObjects(workObjectName, getTypeIconSRC(WORKOBJECT, workObjectName));
+    initTypeDictionaries(default_conf.actors, default_conf.workObjects);
+    addToSelectedActors(actorName, getTypeIconSRC(ACTOR, ACTOR + actorName));
+    addToSelectedWorkObjects(workObjectName, getTypeIconSRC(WORKOBJECT, WORKOBJECT + workObjectName));
 
     const actors = getSelectedActorsDictionary();
     const workObjects = getSelectedWorkObjectsDictionary();
@@ -26,9 +24,8 @@ describe('', function() {
     const workObjectsJSON = json.workObjects;
 
     expect(actorsJSON).to.exist;
-    expect(actorsJSON.Person).to.exist;
+    expect(actorsJSON.Pet).to.exist;
     expect(workObjectsJSON).to.exist;
-    expect(workObjectsJSON.Folder).to.exist;
+    expect(workObjectsJSON.Flag).to.exist;
   });
-
 });
