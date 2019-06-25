9f37870805c536c57f18fdcc7f86a30c8dc8d080
Alexander Zellober <alexander.zellober@wps.de>
2019-06-25 17:16:04 +0200

    fixes error in opening the activity-dialog by double clicking on the number when only one activity with a number is present  master

--- app/app.js
+++ app/app.js
@@ -254,21 +254,23 @@ eventBus.on('element.dblclick', function(e) {
           const width = 25 * zoomX;
           const height = 22 * zoomY;
 
-          for (let i = 0; i<renderedNumberRegistry.length; i++) {
+          for (let i = 1; i<renderedNumberRegistry.length; i++) {
             const currentNum = renderedNumberRegistry[i];
-            const tspan = currentNum.getElementsByTagName('tspan')[0];
-            const tx = tspan.getAttribute('x');
-            const ty = tspan.getAttribute('y');
-            const tNumber = parseInt(tspan.innerHTML);
+            if (currentNum) {
+              const tspan = currentNum.getElementsByTagName('tspan')[0];
+              const tx = tspan.getAttribute('x');
+              const ty = tspan.getAttribute('y');
+              const tNumber = parseInt(tspan.innerHTML);
 
-            const elementX = (tx * zoomX) + (transformX - 5 * zoomX);
-            const elementY = (ty * zoomY) + (transformY - 15 * zoomY);
+              const elementX = (tx * zoomX) + (transformX - 5 * zoomX);
+              const elementY = (ty * zoomY) + (transformY - 15 * zoomY);
 
-            for (let j=0; j<allActivities.length; j++) {
-              let activity = allActivities[j];
-              if (activity.businessObject.number === tNumber) {
-                if (positionsMatch(width, height, elementX, elementY, clickX, clickY)) {
-                  activityDoubleClick(activity);
+              for (let j=0; j<allActivities.length; j++) {
+                let activity = allActivities[j];
+                if (activity.businessObject.number === tNumber) {
+                  if (positionsMatch(width, height, elementX, elementY, clickX, clickY)) {
+                    activityDoubleClick(activity);
+                  }
                 }
               }
             }
