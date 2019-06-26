041d33d6ecf05b60c6b107bcbdda12ab2ee26da8
Alexander Zellober <alexander.zellober@wps.de>
2019-06-26 14:38:40 +0200

    fixes logo when canvas is dirty in released version not showing

--- app/domain-story-modeler/features/export/dirtyFlag.js
+++ app/domain-story-modeler/features/export/dirtyFlag.js
@@ -1,19 +1,22 @@
 'use strict';
 
 let exportButtonIMG = document.getElementById('exportButtonIMG');
+let exportButtonIMGDirty = document.getElementById('exportButtonIMGDirty');
 let dirty = false;
 
 
 export function makeDirty() {
   if (exportButtonIMG) {
-    exportButtonIMG.src = '../../../logo/archive_dirty.png';
+    exportButtonIMGDirty.style = 'display:inherit';
+    exportButtonIMG.style = 'display:none';
   }
   dirty = true;
 }
 
 export function removeDirtyFlag() {
   if (exportButtonIMG) {
-    exportButtonIMG.src = '../../../logo/archive.png';
+    exportButtonIMG.style = 'diplay:inherit';
+    exportButtonIMGDirty.style = 'display:none';
   }
   dirty = false;
 }
