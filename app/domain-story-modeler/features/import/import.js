e2d74d94f49a522526dc606b6b7391adfd648c00
Alexander Zellober <alexander.zellober@wps.de>
2019-08-09 10:03:05 +0200

    moves import-repair functions from import to import-repair

--- app/domain-story-modeler/features/import/import.js
+++ app/domain-story-modeler/features/import/import.js
@@ -1,22 +1,17 @@
 'use strict';
 
+import { DOMAINSTORY, WORKOBJECT, ACTOR } from '../../language/elementTypes';
 import {
-  DOMAINSTORY,
-  ACTIVITY,
-  CONNECTION,
-  WORKOBJECT,
-  ACTOR
-} from '../../language/elementTypes';
-import { checkElementReferencesAndRepair } from './ImportRepair';
+  checkElementReferencesAndRepair,
+  updateCustomElementsPreviousv050,
+  adjustPositions,
+  correctGroupChildren
+} from './ImportRepair';
 import { cleanDictionaries } from '../dictionary/dictionary';
 import {
   correctElementRegitryInit,
-  getAllCanvasObjects,
-  getAllGroups,
   initElementRegistry
 } from '../canvasElements/canvasElementRegistry';
-import { isInDomainStoryGroup } from '../../util/TypeCheck';
-import { assign } from 'min-dash';
 import {
   storyPersistTag,
   loadConfiguration,
@@ -296,105 +291,6 @@ export function configHasChanged(config) {
     }
   }
   return changed;
-}
-
-// when importing a domain-story, the elements that are visually inside a group are not yet associated with it.
-// to ensure they are correctly associated, we add them to the group
-function correctGroupChildren() {
-  const allObjects = getAllCanvasObjects();
-  const groups = getAllGroups();
-
-  groups.forEach(group => {
-    const parent = group.parent;
-    parent.children.slice().forEach(innerShape => {
-      if (innerShape.id != group.id) {
-        if (innerShape.x >= group.x && innerShape.x <= group.x + group.width) {
-          if (
-            innerShape.y >= group.y &&
-            innerShape.y <= group.y + group.height
-          ) {
-            innerShape.parent = group;
-            if (!group.children.includes(innerShape)) {
-              group.children.push(innerShape);
-            }
-          }
-        }
-      }
-    });
-  });
-  allObjects.forEach(shape => {
-    let businessObject = shape.businessObject;
-    if (isInDomainStoryGroup(shape)) {
-      assign(businessObject, {
-        parent: shape.parent.id
-      });
-    }
-  });
-}
-
-/**
- * Ensure backwards compatability.
- * Previously Document had no special name and was just adressed as workObject
- * Bubble was renamed to Conversation
- */
-
-export function updateCustomElementsPreviousv050(elements) {
-  for (let i = 0; i < elements.length; i++) {
-    if (elements[i].type === WORKOBJECT) {
-      elements[i].type = WORKOBJECT + 'Document';
-    } else if (elements[i].type === WORKOBJECT + 'Bubble') {
-      elements[i].type = WORKOBJECT + 'Conversation';
-    }
-  }
-  return elements;
-}
-
-function adjustPositions(elements) {
-  let xLeft, yUp;
-  let isFirst = true;
-
-  elements.forEach(element => {
-    let elXLeft, elYUp;
-    if (element.type != ACTIVITY && element.type != CONNECTION) {
-      if (isFirst) {
-        xLeft = parseFloat(element.x);
-        yUp = parseFloat(element.y);
-        isFirst = false;
-      }
-      elXLeft = parseFloat(element.x);
-      elYUp = parseFloat(element.y);
-      if (elXLeft < xLeft) {
-        xLeft = elXLeft;
-      }
-      if (elYUp < yUp) {
-        yUp = elYUp;
-      }
-    }
-  });
-
-  if (xLeft < 75 || xLeft > 150 || yUp < 0 || yUp > 50) {
-    // add Padding for the Palette and the top
-    xLeft -= 75;
-    yUp -= 50;
-
-    elements.forEach(element => {
-      if (element.type == ACTIVITY || element.type == CONNECTION) {
-        let waypoints = element.waypoints;
-        waypoints.forEach(point => {
-          point.x -= xLeft;
-          point.y -= yUp;
-
-          if (point.original) {
-            point.original.x = point.x;
-            point.original.y = point.y;
-          }
-        });
-      } else {
-        element.x -= xLeft;
-        element.y -= yUp;
-      }
-    });
-  }
 }
 
 function updateIconRegistries(elements) {
