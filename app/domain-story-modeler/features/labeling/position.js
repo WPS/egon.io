0d50723ffddd403d43372409a060a7d5ace885fa
Alexander Zellober <alexander.zellober@wps.de>
2021-06-25 14:25:10 +0200

    #98 allow multiline Input for Activities

--- app/domain-story-modeler/features/labeling/position.js
+++ app/domain-story-modeler/features/labeling/position.js
@@ -2,8 +2,12 @@
 
 import { selectPartOfActivity } from './DSLabelUtil';
 
+export function countLines(str) {
+  return str.split(/\r\n|\r|\n/).length;
+}
+
 // determine the position of the label at the activity
-export function labelPosition(waypoints) {
+export function labelPosition(waypoints, lines = 1) {
   let amountWaypoints = waypoints.length;
   let determinedPosition = {};
   let xPos = 0;
@@ -30,7 +34,7 @@ export function labelPosition(waypoints) {
 
   } else {
     xPos = labelPositionX(waypoints[0], waypoints[1]);
-    yPos = labelPositionY(waypoints[0], waypoints[1]);
+    yPos = labelPositionY(waypoints[0], waypoints[1], lines);
 
     determinedPosition = {
       x: xPos,
@@ -69,13 +73,14 @@ export function labelPositionX(startPoint, endPoint) {
 }
 
 // calculate the Y position of the label
-export function labelPositionY(startPoint, endPoint) {
+export function labelPositionY(startPoint, endPoint, lines) {
   let angle = Math.angleBetween(startPoint, endPoint);
   let offsetY = 0;
   let scaledangle = 0;
 
   if (angle === 0 || angle === 180) {
-    offsetY = 15;
+    offsetY = 15
+    ;
   }
   else if (angle === 90 || angle === 270) {
     offsetY = 0;
@@ -85,7 +90,7 @@ export function labelPositionY(startPoint, endPoint) {
   }
   else if (angle > 90 && angle < 180) { // endpoint in upper left quadrant
     scaledangle = angle - 90;
-    offsetY = -scaledangle / 9;
+    offsetY = -scaledangle / 9 * lines;
   }
   else if (angle > 180 && angle < 270) { // endpoint in lower left quadrant
     scaledangle = angle - 180;
@@ -93,7 +98,8 @@ export function labelPositionY(startPoint, endPoint) {
   }
   else if (angle > 270) { // endpoint in lower right quadrant
     scaledangle = angle - 270;
-    offsetY = -scaledangle / 9;
+    offsetY = -scaledangle / 9 * lines;
   }
+  console.log(offsetY , angle);
   return offsetY + (startPoint.y + endPoint.y)/2;
 }
