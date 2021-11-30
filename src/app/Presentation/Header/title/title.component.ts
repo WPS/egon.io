51bdcb2437aa0954eceee43a0c46fd811a1abb47
alexander.zellober <alexander.zellober@wps.de>
2021-11-30 15:53:44 +0100

    Bugfix: Title-Dialog opened two time when clicking on the Edit Icon

--- src/app/Presentation/Header/title/title.component.ts
+++ src/app/Presentation/Header/title/title.component.ts
@@ -1,16 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {TitleService} from '../../../Service/Title/title.service';
-import {Observable} from 'rxjs';
-import {MatDialogConfig} from '@angular/material/dialog';
-import {HeaderDialogComponent} from '../../Dialog/header-dialog/header-dialog.component';
-import {DialogService} from '../../../Service/Dialog/dialog.service';
+import { Component } from '@angular/core';
+import { TitleService } from '../../../Service/Title/title.service';
+import { Observable } from 'rxjs';
+import { MatDialogConfig } from '@angular/material/dialog';
+import { HeaderDialogComponent } from '../../Dialog/header-dialog/header-dialog.component';
+import { DialogService } from '../../../Service/Dialog/dialog.service';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent {
   title: Observable<string>;
   mouseOver = false;
 
@@ -20,10 +20,6 @@ export class TitleComponent implements OnInit {
   ) {
     this.title = this.titleService.getTitleObservable();
   }
-
-  ngOnInit(): void {
-  }
-
   openHeaderDialog(): void {
     const config = new MatDialogConfig();
     config.disableClose = false;
