92a81a713c0aa20e00fd229f11dc823c1d7c28a4
alexander.zellober <alexander.zellober@wps.de>
2021-11-30 11:15:17 +0100

    Design: Fix CSS for Domain-Customization

--- src/app/Modules/settings.module.ts
+++ src/app/Modules/settings.module.ts
@@ -1,16 +1,18 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {DomainConfigurationComponent} from 'src/app/Presentation/DomainConfiguration/domain-configuration.component';
-import {AutosaveSettingsComponent} from '../Presentation/Autosave/autosave-settings-component/autosave-settings.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {IconListItemComponent} from '../Presentation/DomainConfiguration/icon-list-item/icon-list-item.component';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatListModule} from '@angular/material/list';
-import {DetailsListItemComponent} from '../Presentation/DomainConfiguration/details-list-item/details-list-item.component';
-import {DomainDetailsComponent} from '../Presentation/DomainConfiguration/domain-details/domain-details.component';
-import {GeneralSettingsComponent} from "../Presentation/Settings/general-settings/general-settings.component";
-import {LabelDictionaryComponent} from "../Presentation/Settings/labelDictionary/label-dictionary.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomainConfigurationComponent } from 'src/app/Presentation/DomainConfiguration/domain-configuration.component';
+import { AutosaveSettingsComponent } from '../Presentation/Autosave/autosave-settings-component/autosave-settings.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { IconListItemComponent } from '../Presentation/DomainConfiguration/icon-list-item/icon-list-item.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatListModule } from '@angular/material/list';
+import { DetailsListItemComponent } from '../Presentation/DomainConfiguration/details-list-item/details-list-item.component';
+import { DomainDetailsComponent } from '../Presentation/DomainConfiguration/domain-details/domain-details.component';
+import { GeneralSettingsComponent } from '../Presentation/Settings/general-settings/general-settings.component';
+import { LabelDictionaryComponent } from '../Presentation/Settings/labelDictionary/label-dictionary.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,8 @@ import {LabelDictionaryComponent} from "../Presentation/Settings/labelDictionary
     MatGridListModule,
     MatButtonToggleModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
 })
-export class SettingsModule {
-}
+export class SettingsModule {}
