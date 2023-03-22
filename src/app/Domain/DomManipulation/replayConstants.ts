7b5d96215fb982cf285ceae58582e3151372c9d8
Alexander Zellober <alexander.zellober@wps.de>
2023-03-22 18:17:56 +0100

    decresae number of colour variations

--- src/app/Domain/DomManipulation/replayConstants.ts
+++ src/app/Domain/DomManipulation/replayConstants.ts
@@ -2,7 +2,7 @@ export const NUMBER_BACKGROUND_COLOR = 'white';
 export const NUMBER_COLOR = 'black';
 export const STROKE_WIDTH = '1.5';
 
-export const HIGHLIGHT_NUMBER_BACKGROUNG_COLOR = 'orange';
+export const HIGHLIGHT_NUMBER_BACKGROUNG_COLOR = '#43acbf';
 export const HIGHLIGHT_NUMBER_COLOR = 'black';
 export const HIGHLIGHT_COLOR = 'black';
 export const HIGHLIGHT_STROKE_WIDTH = '4';
