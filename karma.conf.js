6305851ae9a1b484eb366bca7c070f53d646224a
Tom Dymel <tom.dymel@wps.de>
2023-12-20 14:33:26 +0100

    Enable ci tests

--- karma.conf.js
+++ karma.conf.js
@@ -34,8 +34,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
-    singleRun: false,
+    browsers: ["ChromeHeadless"],
+    singleRun: true,
     restartOnFileChange: true,
   });
 };
