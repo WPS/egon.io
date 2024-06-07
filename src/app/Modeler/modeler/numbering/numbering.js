3d996d56d17b8ec83a1b6622f2873e5a5e616ee9
Vadim Holstein <vadim.holstein@wps.de>
2024-06-07 15:18:38 +0200

    [EGON-72]: VH: Run lint format

--- src/app/Modeler/modeler/numbering/numbering.js
+++ src/app/Modeler/modeler/numbering/numbering.js
@@ -19,53 +19,53 @@ export function numberBoxDefinitions(element) {
   let angle = 0;
   if (element.waypoints.length > 1) {
     angle = angleBetween(
-        // Start of first arrow segment
+      // Start of first arrow segment
       element.waypoints[0],
-        // End of first arrow segment
+      // End of first arrow segment
       element.waypoints[1],
     );
   }
   let x = element.waypoints[0].x;
   let y = element.waypoints[0].y;
 
-  let fixedOffsetX = 0
-  let fixedOffsetY = 0
-  let angleDependantOffsetX = 0
-  let angleDependantOffsetY = 0
+  let fixedOffsetX = 0;
+  let fixedOffsetY = 0;
+  let angleDependantOffsetX = 0;
+  let angleDependantOffsetY = 0;
 
   // Fine tune positioning of sequence number above beginning of first arrow segment
   if (angle >= 0 && angle <= 45) {
-    fixedOffsetX = 25
-    angleDependantOffsetY = 20 * (1 - angle / 45)
+    fixedOffsetX = 25;
+    angleDependantOffsetY = 20 * (1 - angle / 45);
   } else if (angle <= 90) {
-    fixedOffsetX = 5
-    angleDependantOffsetX = 15 * (1 - (angle - 45) / 45)
+    fixedOffsetX = 5;
+    angleDependantOffsetX = 15 * (1 - (angle - 45) / 45);
   } else if (angle <= 135) {
-    fixedOffsetX = 5
-    angleDependantOffsetX = -20 * ((angle - 90) / 45)
+    fixedOffsetX = 5;
+    angleDependantOffsetX = -20 * ((angle - 90) / 45);
   } else if (angle <= 180) {
-    fixedOffsetX = -15
-    angleDependantOffsetY = 20 * ((angle - 135) / 45)
+    fixedOffsetX = -15;
+    angleDependantOffsetY = 20 * ((angle - 135) / 45);
   } else if (angle <= 225) {
-    fixedOffsetX = -15
-    fixedOffsetY = 15
-    angleDependantOffsetY = 25 * ((angle - 180) / 45)
+    fixedOffsetX = -15;
+    fixedOffsetY = 15;
+    angleDependantOffsetY = 25 * ((angle - 180) / 45);
   } else if (angle <= 270) {
-    fixedOffsetX = 5
-    angleDependantOffsetX = -20 * (1 - (angle - 225) / 45)
-    fixedOffsetY = 40
+    fixedOffsetX = 5;
+    angleDependantOffsetX = -20 * (1 - (angle - 225) / 45);
+    fixedOffsetY = 40;
   } else if (angle <= 315) {
-    fixedOffsetX = 5
-    angleDependantOffsetX = 25 * ((angle - 270) / 45)
-    fixedOffsetY = 40
+    fixedOffsetX = 5;
+    angleDependantOffsetX = 25 * ((angle - 270) / 45);
+    fixedOffsetY = 40;
   } else {
-    fixedOffsetX = 25
-    fixedOffsetY = 20
-    angleDependantOffsetY = 15 * (1 - (angle - 315) / 45)
+    fixedOffsetX = 25;
+    fixedOffsetY = 20;
+    angleDependantOffsetY = 15 * (1 - (angle - 315) / 45);
   }
 
-  x = x + fixedOffsetX + angleDependantOffsetX
-  y = y + fixedOffsetY + angleDependantOffsetY
+  x = x + fixedOffsetX + angleDependantOffsetX;
+  y = y + fixedOffsetY + angleDependantOffsetY;
 
   return {
     textAlign: alignment,
