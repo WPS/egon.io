6ea5af72c9f7643c35cf8fa06c121e722499ad3d
Jannik Friemann <jannik.friemann@wps.de>
2024-07-29 10:07:24 +0200

    [EGON-68] Don't override color of highlighted arrows

--- src/app/Domain/DomManipulation/replayConstants.ts
+++ src/app/Domain/DomManipulation/replayConstants.ts
@@ -4,5 +4,4 @@ export const STROKE_WIDTH = '1.5';
 
 export const HIGHLIGHT_NUMBER_BACKGROUNG_COLOR = '#43acbf';
 export const HIGHLIGHT_NUMBER_COLOR = 'black';
-export const HIGHLIGHT_COLOR = 'black';
 export const HIGHLIGHT_STROKE_WIDTH = '4';
