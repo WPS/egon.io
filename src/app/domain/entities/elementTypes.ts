c20727a9be81a10f003fda6277e0849c5700643e
hofstef <sh@wps.de>
2024-08-06 11:40:15 +0200

    added documentation for edge case

--- src/app/domain/entities/elementTypes.ts
+++ src/app/domain/entities/elementTypes.ts
@@ -19,10 +19,14 @@ export namespace ElementTypes {
     return '';
   }
 
+  // this check will give the wrong result for imported domain stories created with <= 1.3.x
+  // because the "-custom" suffix for custom icons was introduced with a later version
   export function isCustomType(type: string): boolean {
     return type.endsWith(ElementTypes.CUSTOM);
   }
 
+  // this check will give the wrong result for imported domain stories created with <= 1.3.x
+  // because the "-custom" suffix for custom icons was introduced with a later version
   export function isCustomSvgType(type: string) {
     return type.endsWith('_svg' + ElementTypes.CUSTOM);
   }
