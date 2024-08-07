98302fa16b7e8711f649a6eec12f6e27481c7396
Abdullatif Kikhia <abdullatif.kikhia@wps.de>
2024-08-07 14:25:34 +0200

    [EGON-68] builds popup for Url upload and sets file name

---
+++ src/app/tools/import/presentation/import-dialog/import-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+@Component({
+  selector: 'app-import-dialog',
+  templateUrl: './import-dialog.component.html',
+  styleUrls: ['./import-dialog.component.scss'],
+})
+export class ImportDialogComponent implements OnInit {
+  fn: any;
+  fileUrl: BehaviorSubject<string>;
+
+  constructor(
+    private dialogRef: MatDialogRef<ImportDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data: () => {},
+  ) {
+    this.fileUrl = new BehaviorSubject<string>('');
+    this.fn = data;
+  }
+
+  ngOnInit(): void {}
+
+  doImport(): void {
+    this.fn(this.fileUrl.value);
+    this.close();
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  updateUrl($event: Event) {
+    // @ts-ignore
+    this.fileUrl.next($event.target.value);
+  }
+}
