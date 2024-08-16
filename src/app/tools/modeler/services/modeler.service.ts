5af3ed2dabb2ee35e40a8d0addaca98115f016da
hofstef <sh@wps.de>
2024-08-16 10:48:50 +0200

    fix linting error

--- src/app/tools/modeler/services/modeler.service.ts
+++ src/app/tools/modeler/services/modeler.service.ts
@@ -113,7 +113,7 @@ export class ModelerService {
     if (version !== null && version !== environment.version) {
       this.snackbar
         .open(
-          'Egon was updated. Clear your browser\'s local storage.',
+          "Egon was updated. Clear your browser's local storage.",
           'More information',
           {
             duration: SNACKBAR_DURATION_LONGER,
