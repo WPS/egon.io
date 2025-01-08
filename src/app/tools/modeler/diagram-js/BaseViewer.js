af3eaea8009dfa67125fafdb425c926e33a62384
Hanna Tormählen <hanna.tormaehlen@wps.de>
2025-01-08 17:12:54 +0100

    adds bpmn logo again, WIP

--- src/app/tools/modeler/diagram-js/BaseViewer.js
+++ src/app/tools/modeler/diagram-js/BaseViewer.js
@@ -1,20 +1,28 @@
-import Diagram from 'diagram-js';
-
+/**
+ * The code in the <project-logo></project-logo> area
+ * must not be changed.
+ *
+ * @see http://bpmn.io/license for more information.
+ */
 import {
   assign,
+  find,
   isNumber,
   omit
 } from 'min-dash';
 
 import {
   domify,
+  assignStyle,
   query as domQuery,
-  remove as domRemove,
+  remove as domRemove
 } from 'min-dom';
 
 import {
   innerSVG
 } from 'tiny-svg';
+
+import Diagram from 'diagram-js';
 
 import inherits from 'inherits-browser';
 
@@ -24,94 +32,54 @@ export default function BaseViewer(options) {
 
   this._container = this._createContainer(options);
 
+  /* <project-logo> */
+  addProjectLogo(this._container);
+  /* </project-logo> */
+
   this._init(this._container, options);
 }
 
 inherits(BaseViewer, Diagram);
 
-BaseViewer.prototype._init = function(container, options) {
-
-  var baseModules = options.modules || this.getModules(),
-    additionalModules = options.additionalModules || [],
-    staticModules = [
-      {
-        bpmnjs: [ 'value', this ] //TODO
-      }
-    ];
-
-  var diagramModules = [].concat(staticModules, baseModules, additionalModules);
-
-  var diagramOptions = assign(omit(options, [ 'additionalModules' ]), {
-    canvas: assign({}, options.canvas, { container: container }),
-    modules: diagramModules
-  });
-
-  // invoke diagram constructor
-  Diagram.call(this, diagramOptions);
-
-  if (options && options.container) {
-    this.attachTo(options.container);
-  }
-};
-
-BaseViewer.prototype._createContainer = function(options) {
-
-  var container = domify('<div class="egon-container"></div>');
-
-  assign(container.style, {
-    width: ensureUnit(options.width),
-    height: ensureUnit(options.height),
-    position: options.position
-  });
-
-  return container;
-};
-
-BaseViewer.prototype.attachTo = function(parentNode) {
-
-  if (!parentNode) {
-    throw new Error('parentNode required');
-  }
-
-  // ensure we detach from the
-  // previous, old parent
-  this.detach();
-
-  // unwrap jQuery if provided
-  if (parentNode.get && parentNode.constructor.prototype.jquery) {
-    parentNode = parentNode.get(0);
-  }
-
-  if (typeof parentNode === 'string') {
-    parentNode = domQuery(parentNode);
-  }
-
-  parentNode.appendChild(this._container);
-
-  this._emit('attach', {});
-
-  this.get('canvas').resized();
-};
-
-BaseViewer.prototype.detach = function() {
-
-  const container = this._container,
-    parentNode = container.parentNode;
-
-  if (!parentNode) {
-    return;
-  }
-
-  /**
-   * A `detach` event.
-   *
-   * @event BaseViewer#DetachEvent
-   * @type {Object}
-   */
-  this._emit('detach', {});
-
-  parentNode.removeChild(container);
-};
+//TODO-RIP_BPMN do we need this?
+// BaseViewer.prototype.open = async function open(bpmnDiagramOrId) {
+//
+//   const definitions = this._definitions;
+//   let bpmnDiagram = bpmnDiagramOrId;
+//
+//   if (!definitions) {
+//     const error = new Error('no XML imported');
+//     addWarningsToError(error, []);
+//
+//     throw error;
+//   }
+//
+//   if (typeof bpmnDiagramOrId === 'string') {
+//     bpmnDiagram = findBPMNDiagram(definitions, bpmnDiagramOrId);
+//
+//     if (!bpmnDiagram) {
+//       const error = new Error('BPMNDiagram <' + bpmnDiagramOrId + '> not found');
+//       addWarningsToError(error, []);
+//
+//       throw error;
+//     }
+//   }
+//
+//   // clear existing rendered diagram
+//   // catch synchronous exceptions during #clear()
+//   try {
+//     this.clear();
+//   } catch (error) {
+//     addWarningsToError(error, []);
+//
+//     throw error;
+//   }
+//
+//   // perform graphical import
+//   const { warnings } = await importBpmnDiagram(this, definitions, bpmnDiagram);
+//
+//   return { warnings };
+// };
 
 BaseViewer.prototype.saveSVG = async function saveSVG() {
   this._emit('saveSVG.start');
@@ -142,12 +110,6 @@ BaseViewer.prototype.saveSVG = async function saveSVG() {
     err = e;
   }
 
-  /**
-   * A `saveSVG.done` event.
-   *
-   * @event BaseViewer#SaveSVGDoneEvent
-   * @type {SaveSVGDoneEvent}
-   */
   this._emit('saveSVG.done', {
     error: err,
     svg: svg
@@ -160,16 +122,25 @@ BaseViewer.prototype.saveSVG = async function saveSVG() {
   return { svg };
 };
 
+//TODO-RIP_BPMN do we need this?
+
+// BaseViewer.prototype._setDefinitions = function(definitions) {
+//   this._definitions = definitions;
+// };
+
 BaseViewer.prototype.getModules = function() {
   return this._modules;
 };
 
-BaseViewer.prototype._emit = function(type, event) {
-  return this.get('eventBus').fire(type, event);
-};
+BaseViewer.prototype.clear = function() {
+  if (!this.getDefinitions()) {
 
-BaseViewer.prototype.on = function(events, priority, callback, that) {
-  return this.get('eventBus').on(events, priority, callback, that);
+    // no diagram to clear
+    return;
+  }
+
+  // remove drawn elements
+  Diagram.prototype.clear.call(this);
 };
 
 BaseViewer.prototype.destroy = function() {
@@ -181,12 +152,212 @@ BaseViewer.prototype.destroy = function() {
   domRemove(this._container);
 };
 
-function ensureUnit(val) {
-  return val + (isNumber(val) ? 'px' : '');
+BaseViewer.prototype.on = function(events, priority, callback, that) {
+  return this.get('eventBus').on(events, priority, callback, that);
+};
+
+BaseViewer.prototype.off = function(events, callback) {
+  this.get('eventBus').off(events, callback);
+};
+
+BaseViewer.prototype.attachTo = function(parentNode) {
+
+  if (!parentNode) {
+    throw new Error('parentNode required');
+  }
+
+  // ensure we detach from the
+  // previous, old parent
+  this.detach();
+
+  // unwrap jQuery if provided
+  if (parentNode.get && parentNode.constructor.prototype.jquery) {
+    parentNode = parentNode.get(0);
+  }
+
+  if (typeof parentNode === 'string') {
+    parentNode = domQuery(parentNode);
+  }
+
+  parentNode.appendChild(this._container);
+
+  this._emit('attach', {});
+
+  this.get('canvas').resized();
+};
+
+//TODO-RIP_BPMN do we need this?
+
+// BaseViewer.prototype.getDefinitions = function() {
+//   return this._definitions;
+// };
+
+BaseViewer.prototype.detach = function() {
+
+  const container = this._container,
+    parentNode = container.parentNode;
+
+  if (!parentNode) {
+    return;
+  }
+  this._emit('detach', {});
+
+  parentNode.removeChild(container);
+};
+
+BaseViewer.prototype._init = function(container, options) {
+
+  const baseModules = options.modules || this.getModules(options),
+    additionalModules = options.additionalModules || [],
+    staticModules = [
+      {
+        bpmnjs: [ 'value', this ]
+      }
+    ];
+
+  const diagramModules = [].concat(staticModules, baseModules, additionalModules);
+
+  const diagramOptions = assign(omit(options, [ 'additionalModules' ]), {
+    canvas: assign({}, options.canvas, { container: container }),
+    modules: diagramModules
+  });
+
+  // invoke diagram constructor
+  Diagram.call(this, diagramOptions);
+
+  if (options && options.container) {
+    this.attachTo(options.container);
+  }
+};
+
+BaseViewer.prototype._emit = function(type, event) {
+  return this.get('eventBus').fire(type, event);
+};
+
+BaseViewer.prototype._createContainer = function(options) {
+
+  const container = domify('<div class="egon-container"></div>');
+
+  assignStyle(container, {
+    width: ensureUnit(options.width),
+    height: ensureUnit(options.height),
+    position: options.position
+  });
+
+  return container;
+};
+
+BaseViewer.prototype._modules = [];
+
+// helpers ///////////////
+
+function addWarningsToError(err, warningsAry) {
+  err.warnings = warningsAry;
+  return err;
 }
 
-var DEFAULT_OPTIONS = {
+function checkValidationError(err) {
+
+  // check if we can help the user by indicating wrong BPMN 2.0 xml
+  // (in case he or the exporting tool did not get that right)
+
+  const pattern = /unparsable content <([^>]+)> detected([\s\S]*)$/;
+  const match = pattern.exec(err.message);
+
+  if (match) {
+    err.message =
+      'unparsable content <' + match[1] + '> detected; ' +
+      'this may indicate an invalid BPMN 2.0 diagram file' + match[2];
+  }
+
+  return err;
+}
+
+const DEFAULT_OPTIONS = {
   width: '100%',
   height: '100%',
   position: 'relative'
 };
+
+
+/**
+ * Ensure the passed argument is a proper unit (defaulting to px)
+ */
+function ensureUnit(val) {
+  return val + (isNumber(val) ? 'px' : '');
+}
+
+//TODO-RIP_BPMN do we need this?
+
+// /**
+//  * Find BPMNDiagram in definitions by ID
+//  *
+//  * @param {ModdleElement<Definitions>} definitions
+//  * @param {string} diagramId
+//  *
+//  * @return {ModdleElement<BPMNDiagram>|null}
+//  */
+// function findBPMNDiagram(definitions, diagramId) {
+//   if (!diagramId) {
+//     return null;
+//   }
+//
+//   return find(definitions.diagrams, function(element) {
+//     return element.id === diagramId;
+//   }) || null;
+// }
+
+
+/* <project-logo> */
+
+import {
+  open as openPoweredBy,
+  BPMNIO_IMG,
+  LOGO_STYLES,
+  LINK_STYLES
+} from './copiedClasses/PoweredByUtil';
+
+import {
+  event as domEvent
+} from 'min-dom';
+
+/**
+ * Adds the project logo to the diagram container as
+ * required by the bpmn.io license.
+ *
+ * @see http://bpmn.io/license
+ *
+ * @param {Element} container
+ */
+function addProjectLogo(container) {
+  const img = BPMNIO_IMG;
+
+  const linkMarkup =
+    '<a href="http://bpmn.io" ' +
+    'target="_blank" ' +
+    'class="bjs-powered-by" ' +
+    'title="Powered by bpmn.io" ' +
+    '>' +
+    img +
+    '</a>';
+
+  const linkElement = domify(linkMarkup);
+
+  assignStyle(domQuery('svg', linkElement), LOGO_STYLES);
+  assignStyle(linkElement, LINK_STYLES, {
+    position: 'absolute',
+    bottom: '15px',
+    right: '15px',
+    zIndex: '100'
+  });
+
+  container.appendChild(linkElement);
+
+  domEvent.bind(linkElement, 'click', function(event) {
+    openPoweredBy();
+
+    event.preventDefault();
+  });
+}
+
+/* </project-logo> */
