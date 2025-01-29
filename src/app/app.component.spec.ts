0d5a42e8b706a765ed18b05149f9e14b52ef2f33
hofstef <sh@wps.de>
2025-01-29 21:49:25 +0100

    remove unused code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -42,7 +42,6 @@ describe('AppComponent', () => {
 
   it('should load latest draft', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     fixture.detectChanges();
     expect(autosaveService.loadLatestDraft).toHaveBeenCalled();
   });
