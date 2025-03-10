0a7d1dd6e372a0f06fd26719fa462c46300be87f
Peter Heinemann <peter.heinemann@miragon.io>
2025-03-10 19:29:45 +0100

    chore: update vite.config.ts

--- monorepo/egon/apps/vscode/egon-modeler-webview/vite.config.ts
+++ monorepo/egon/apps/vscode/egon-modeler-webview/vite.config.ts
@@ -1,30 +1,32 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { defineConfig } from "vite";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
 
 export default defineConfig({
     root: __dirname,
-    cacheDir: '../../../node_modules/.vite/apps/vscode/egon-modeler-webview',
+    cacheDir: "../../../node_modules/.vite/apps/vscode/egon-modeler-webview",
     server: {
         port: 4200,
-        host: 'localhost',
+        host: "localhost",
     },
     preview: {
         port: 4300,
-        host: 'localhost',
+        host: "localhost",
     },
-    plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-    // Uncomment this if you are using workers.
-    // worker: {
-    //  plugins: [ nxViteTsPaths() ],
-    // },
+    plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
     build: {
-        outDir: '../../../dist/apps/vscode/egon-modeler-webview',
+        outDir: "../../../dist/apps/vscode/egon-modeler-webview",
         emptyOutDir: true,
         reportCompressedSize: true,
         commonjsOptions: {
             transformMixedEsModules: true,
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`,
+            },
         },
     },
 });
