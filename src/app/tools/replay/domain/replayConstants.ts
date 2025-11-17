35486e7188c3fe2c4809c39acb141244938e3821
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2025-11-17 14:43:22 +0100

    highlight current activity label during replay

--- src/app/tools/replay/domain/replayConstants.ts
+++ src/app/tools/replay/domain/replayConstants.ts
@@ -1,7 +1,9 @@
+export const LABEL_FONT_WEIGHT = 'normal';
 export const NUMBER_BACKGROUND_COLOR = 'white';
 export const NUMBER_COLOR = 'black';
 export const STROKE_WIDTH = '1.5';
 
+export const HIGHLIGHT_LABEL_FONT_WEIGHT = 'bold';
 export const HIGHLIGHT_NUMBER_BACKGROUND_COLOR = '#a4d7e1';
 export const HIGHLIGHT_NUMBER_COLOR = 'black';
 export const HIGHLIGHT_STROKE_WIDTH = '4';
