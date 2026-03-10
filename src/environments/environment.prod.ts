07e2bda0e5ae8f4898f11be5cc72c8b3269ff6c2
hofstef <sh@wps.de>
2026-03-10 16:54:15 +0100

    bump up version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: '3.1.0',
+  version: '3.1.1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: '3.1.0',
+  version: '3.1.1-dev',
 };
 
 /*
