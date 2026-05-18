ca1a997c494fa7235698c98703b5cd98244f5238
Alexander Zellober <alexander.zellober@wps.de>
2026-05-18 10:09:56 +0200

    migrate to angular 21 - part 1

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
@@ -9,6 +9,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((err) =>
   console.error(err),
 );
